f6fcfe24549d1dfa7a05834849f623eab065539d
Diocleniax789 <julioramdom123@gmail.com>
2024-07-16 14:59:27 -0300

    se añadio el menu principal

--- ahorcado_game.pas
+++ ahorcado_game.pas
@@ -4,6 +4,35 @@ USES crt;
 VAR
 
 
+
+
+
+PROCEDURE menu_principal;
+VAR
+ opcion: integer;
+ BEGIN
+  REPEAT
+  clrscr;
+  textcolor(white);
+  writeln('MENU');
+  writeln('----');
+  writeln('1. Cargar palabra');
+  writeln('2. Jugar');
+  writeln('3. Salir');
+  writeln();
+  write('Seleccione opcion: ');
+  readln(opcion);
+  CASE opcion OF
+       1:BEGIN
+         cargar_palabra;
+         END;
+       2:BEGIN
+         jugar;
+         END;
+  END;
+  UNTIL (opcion = 3);
+ END;
+
 BEGIN
 
 
